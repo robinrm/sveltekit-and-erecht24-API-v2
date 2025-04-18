3c7d5a63a5602a6053fd04b2619d41221a67b11a
robinrm <dev@robinrm.de>
2025-04-18 21:06:07 +0200

    updated helmet to allow erecht API v2

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,10 +7,18 @@ import helmet from "sveltekit-helmet";
 export const handle = helmet({
     contentSecurityPolicy: {
         directives: {
+            defaultSrc: [
+                "'self'",
+                "https://api.e-recht24.de/v2/imprint",
+                "https://api.e-recht24.de/v2/privacyPolicy",
+            ],
             scriptSrc: [
                 "'self'",
                 "'unsafe-inline'", // Allow SvelteKit hot reload
+                "https://api.e-recht24.de/v2/imprint",
+                "https://api.e-recht24.de/v2/privacyPolicy",
             ],
+            objectSrc: ["'self'"],
         },
     },
 });
